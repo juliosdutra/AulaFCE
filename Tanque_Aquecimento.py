e447d886d0899c637ebd530f709e3046ce24511b
Julio Dutra <61989878+juliosdutra@users.noreply.github.com>
2026-05-25 17:33:41 -0300

    Fix temperature change rate calculation

--- Tanque_Aquecimento.py
+++ Tanque_Aquecimento.py
@@ -62,7 +62,7 @@ def modelo_aquecimento(t, T, params):
     m = params.m
     Cp = params.Cp
 
-    dTdt = (U * A * (Tamb - T) + q) / (m * Cp)
+    dTdt = (q - U * A * (Tamb - T)) / (m * Cp)
 
     return dTdt
 
